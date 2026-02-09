779c9876c87b77c788fca855803cca1a5f1f354a
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2026-02-09 04:39:08 +0100

    use extern C on build syms

--- src/build.cpp
+++ src/build.cpp
@@ -5,27 +5,27 @@
 #define EXPAND_AND_STRINGIFY(s) STRINGIFY(s)
 
 #ifdef PACKAGE_AUTHOR
-const wchar_t gBuildAuthor[] = EXPAND_AND_STRINGIFY(PACKAGE_AUTHOR);
+extern "C" const wchar_t gBuildAuthor[] = EXPAND_AND_STRINGIFY(PACKAGE_AUTHOR);
 #else
-const wchar_t gBuildAuthor[] = L"???";
+extern "C" const wchar_t gBuildAuthor[] = L"???";
 #endif
 
 #ifdef PACKAGE_COMMIT_AUTHOR
-const wchar_t gCommitAuthor[] = EXPAND_AND_STRINGIFY(PACKAGE_COMMIT_AUTHOR);
+extern "C" const wchar_t gCommitAuthor[] = EXPAND_AND_STRINGIFY(PACKAGE_COMMIT_AUTHOR);
 #else
-const wchar_t gCommitAuthor[] = L"???";
+extern "C" const wchar_t gCommitAuthor[] = L"???";
 #endif
 
-const wchar_t gBuildDate[] = LITTLEENDIANIFY(__DATE__) L" " LITTLEENDIANIFY(__TIME__);
+extern "C" const wchar_t gBuildDate[] = LITTLEENDIANIFY(__DATE__) L" " LITTLEENDIANIFY(__TIME__);
 
 #ifdef PACKAGE_VERSION
-const wchar_t gBuildGitVersion[] = EXPAND_AND_STRINGIFY(PACKAGE_VERSION);
+extern "C" const wchar_t gBuildGitVersion[] = EXPAND_AND_STRINGIFY(PACKAGE_VERSION);
 #else
-const wchar_t gBuildGitVersion[] = L"???";
+extern "C" const wchar_t gBuildGitVersion[] = L"???";
 #endif
 
 #ifdef PACKAGE_NAME
-const wchar_t gCommitGitString[] = EXPAND_AND_STRINGIFY(PACKAGE_NAME);
+extern "C" const wchar_t gCommitGitString[] = EXPAND_AND_STRINGIFY(PACKAGE_NAME);
 #else
-const wchar_t gCommitGitString[] = L"???";
+extern "C" const wchar_t gCommitGitString[] = L"???";
 #endif
